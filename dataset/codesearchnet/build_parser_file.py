24f9fd68f0b64496c6e1f88dabf7f8dd59794798
Yao <wanyao1992@gmail.com>
2020-04-14 00:10:40 +0800

    data prepare script

--- dataset/codesearchnet/build_parser_file.py
+++ dataset/codesearchnet/build_parser_file.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
-
-from __future__ import absolute_import, print_function, division
-
 from tree_sitter import Language
 
 Language.build_library(
-    '/data/wanyao/yang/ghproj_d/GitHub/naturalcodev2/dataset/parser_zips/languages.so',  # your language parser file
+    '/data/wanyao/Dropbox/ghproj-titan/naturalcodev3/dataset/codesearchnet/ruby.so',  # your language parser file
 
     # Include one or more languages
     [
-        # '/data/wanyao/yang/ghproj_d/GitHub/tree_sitter/tree-sitter-c-master',
-        # '/data/wanyao/yang/ghproj_d/GitHub/tree_sitter/tree-sitter-cpp-master',
-        # '/data/wanyao/yang/ghproj_d/GitHub/tree_sitter/tree-sitter-c-sharp-master',
-        '/data/wanyao/yang/ghproj_d/GitHub/tree_sitter/tree-sitter-go-master',
-        '/data/wanyao/yang/ghproj_d/GitHub/tree_sitter/tree-sitter-python-master',
-        '/data/wanyao/yang/ghproj_d/GitHub/tree_sitter/tree-sitter-java-master',
-        '/data/wanyao/yang/ghproj_d/GitHub/tree_sitter/tree-sitter-javascript-master',
-        '/data/wanyao/yang/ghproj_d/GitHub/tree_sitter/tree-sitter-ruby-master',
-        '/data/wanyao/yang/ghproj_d/GitHub/tree_sitter/tree-sitter-php-master',
+        # '/data/wanyao/ghproj_d/naturalcodev3/codesearchnet/tree-sitter/tree-sitter-c-master',
+        # '/data/wanyao/ghproj_d/naturalcodev3/codesearchnet/tree-sitter/tree-sitter-cpp-master',
+        # '/data/wanyao/ghproj_d/naturalcodev3/codesearchnet/tree-sitter /tree-sitter-c-sharp-master',
+        # '/data/wanyao/ghproj_d/naturalcodev3/codesearchnet/tree-sitter/tree-sitter-go-master',
+        # '/data/wanyao/ghproj_d/naturalcodev3/codesearchnet/tree-sitter/tree-sitter-python-master',
+        # '/data/wanyao/ghproj_d/naturalcodev3/codesearchnet/tree-sitter/tree-sitter-java-master',
+        # '/data/wanyao/ghproj_d/naturalcodev3/codesearchnet/tree-sitter/tree-sitter-javascript-master',
+        '/data/wanyao/ghproj_d/naturalcodev3/codesearchnet/tree-sitter/tree-sitter-ruby-master',
+        # '/data/wanyao/ghproj_d/naturalcodev3/codesearchnet/tree-sitter/tree-sitter-php-master',
     ]
 )
+print('exit...')
