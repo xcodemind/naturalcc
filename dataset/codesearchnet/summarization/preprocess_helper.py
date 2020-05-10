077c7c41d84b90bbcfd24764ee31147121136ecc
anonym4research <AAAAAAAanonymous@protonmail.com>
2020-05-10 11:41:23 +1000

    Update preprocess_helper.py

--- dataset/codesearchnet/summarization/preprocess_helper.py
+++ dataset/codesearchnet/summarization/preprocess_helper.py
@@ -1,12 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import os
 import ujson
-import os
-
 
 S_SEP = '<S_SEP>'
 S_CLS = '<CLS>'
 S_MASK = '<MASK>'
-output_prefixs=['train','valid','test'] # insert spec tokens for all datasets.
+output_prefixs = ['train', 'valid', 'test']  # insert spec tokens for all datasets.
 PREPROCESS = 'preprocess'
 prefix = 'pref'
 
@@ -21,6 +21,7 @@ def file_name(prefix, lang):
 def dest_path(args, prefix, lang):
     return os.path.join(args['preprocess']['destdir'], file_name(prefix, lang))
 
+
 def insert_sep_token(args, input_prefix, output_prefix, lang):
     output_text_file = dest_path(args,
                                  output_prefix,
@@ -33,16 +34,16 @@ def insert_sep_token(args, input_prefix, output_prefix, lang):
             with open(file_name(input_prefix, lang), 'r') as input_file:
                 for line in input_file.readlines():
                     ln = ujson.loads(line)
-                    for count in range(10,1,-1):  # to handle duplicate '\n'
+                    for count in range(10, 1, -1):  # to handle duplicate '\n'
                         ln = ln.replace('\n', S_SEP, count)
-                        ln=S_CLS +ln # profix <CLS>
+                        ln = S_CLS + ln  # profix <CLS>
                     output_file.write(ujson.dumps(ln) + '\n')
     else:
         raise ValueError(
             "dataset 'code' supported only 2020.0506"
         )
 
-def insert_sep_tokens(args,lang='code'):
-    for idx in output_prefixs:
-        insert_sep_token(args,args[PREPROCESS][idx+prefix],idx,lang)
 
+def insert_sep_tokens(args, lang='code'):
+    for idx in output_prefixs:
+        insert_sep_token(args, args[PREPROCESS][idx + prefix], idx, lang)
