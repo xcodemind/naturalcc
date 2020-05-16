0731a5a251ce76157478f75f83333fd31cc173bf
Yao <wanyao1992@gmail.com>
2020-05-16 18:00:27 +0800

    debug preprocess_hicodebert binary

--- dataset/codesearchnet/summarization/multiprocessing_wordpiece_encoder.py
+++ dataset/codesearchnet/summarization/multiprocessing_wordpiece_encoder.py
@@ -4,62 +4,56 @@
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-# comand: python -m dataset.codesearchnet.summarization.multiprocessing_wordpiece_encoder --inputs ~/.ncc/CodeSearchNet/flatten/ruby/train.code --model-prefix ~/.ncc/CodeSearchNet/summarization/hicodebert-data-bin/wordpiece_bpe/codesearchnet --outputs ~/.ncc/CodeSearchNet/summarization/hicodebert-data-bin/codesearchnet.train.bpe --keep-empty --format piece --vocab-size 50000 --insert --workers 40
-import argparse
 import contextlib
 import sys
-
+import os
 from collections import Counter
 from multiprocessing import Pool
-
-# from ncc.data.encoders.gpt2_bpe_utils import get_encoder
+from collections import namedtuple
 import sentencepiece as spm
 import ujson
 import re
-# S_SEP = '<S_SEP>'
-# CLS = '<CLS>'
 from ncc.data import constants
+from ncc import LOGGER
+from ncc.utils.util_file import load_yaml
+
+INSERTED_SUFFIX = '_inserted'
+
 
 def build_model(input, model_prefix, vocab_size):
     spm.SentencePieceTrainer.Train('--input={} --model_prefix={} --vocab_size={} --user_defined_symbols={},{}'.format(input, model_prefix, vocab_size, constants.CLS, constants.S_SEP))
-    # spm.SentencePieceTrainer.Train('--input={} --model_prefix={} --vocab_size={}'.format(input, model_prefix, vocab_size))
 
 
-def insert_sep_token(input_file):
-    # output_text_file = dest_path(args,
-    #                              output_prefix,
-    #                              # + ".{}-{}".format(args['preprocess']['source_lang'], args['preprocess']['target_lang'])
-    #                              lang,
-    #                              )
-    with open(input_file + '_inserted', 'w') as out_file:
+def insert_sep_token(input_file, inserted_suffix):
+    # print('input_file + inserted_suffix: ', type(input_file + inserted_suffix))
+    with open(input_file + inserted_suffix, 'w') as out_file:
         with open(input_file, 'r') as in_file:
             for line in in_file.readlines():
                 ln = ujson.loads(line)
-                # for count in range(10, 1, -1):  # to handle duplicate '\n'
-                #     ln = ln.replace('\n', S_SEP, count)
                 ln = re.sub('\n\s*\n', '\n', ln)  # remove "\n \n" -> \n
                 ln = ln.replace('\n', ' ' + constants.S_SEP + ' ')  # should be whitespace before and after S_SEP
                 ln = constants.CLS + ' ' + ln  # profix <CLS>, should be whitespace after the CLS
                 out_file.write(ujson.dumps(ln) + '\n')
 
+
 def main(args):
-    assert len(args.inputs) == len(args.outputs), \
+    assert len(args['preprocess']['inputs']) == len(args['preprocess']['outputs']), \
         "number of input and output paths should match"
 
     with contextlib.ExitStack() as stack:
         inputs = [
             stack.enter_context(open(input, "r", encoding="utf-8"))
             if input != "-" else sys.stdin
-            for input in args.inputs
+            for input in args['preprocess']['inputs']
         ]
         outputs = [
             stack.enter_context(open(output, "w", encoding="utf-8"))
             if output != "-" else sys.stdout
-            for output in args.outputs
+            for output in args['preprocess']['outputs']
         ]
 
         encoder = MultiprocessingEncoder(args)
-        pool = Pool(args.workers, initializer=encoder.initializer)
+        pool = Pool(args['preprocess']['workers'], initializer=encoder.initializer)
         encoded_lines = pool.imap(encoder.encode_lines, zip(*inputs), 100)
 
         stats = Counter()
@@ -83,25 +77,24 @@ class MultiprocessingEncoder(object):
 
     def initializer(self):
         global sp
-        # bpe = get_encoder(self.args.encoder_json, self.args.vocab_bpe)
         sp = spm.SentencePieceProcessor()
-        sp.Load('{}.model'.format(self.args.model_prefix))
+        sp.Load('{}.model'.format(self.args['preprocess']['model_prefix']))
 
     def encode(self, line):
         global sp
-        if self.args.format == 'id':
+        if self.args['preprocess']['format'] == 'id':
             ids = sp.EncodeAsIds(line)
             return list(map(str, ids))
-        elif self.args.format == 'piece':
+        elif self.args['preprocess']['format'] == 'piece':
             pieces = sp.EncodeAsPieces(line)
             return pieces
 
     def decode(self, tokens):
         global sp
         # return bpe.decode(tokens)
-        if self.args.format == 'id':
+        if self.args['preprocess']['format'] == 'id':
             return sp.DecodeIds(tokens)
-        elif self.args.format == 'piece':
+        elif self.args['preprocess']['format'] == 'piece':
             return sp.DecodePieces(tokens)
 
     def encode_lines(self, lines):
@@ -111,7 +104,8 @@ class MultiprocessingEncoder(object):
         enc_lines = []
         for line in lines:
             line = line.strip()
-            if len(line) == 0 and not self.args.keep_empty:
+            line = ujson.loads(line)
+            if len(line) == 0 and not self.args['preprocess']['keep_empty']:
                 return ["EMPTY", None]
             tokens = self.encode(line)
             enc_lines.append(" ".join(tokens))
@@ -126,74 +120,27 @@ class MultiprocessingEncoder(object):
 
 
 if __name__ == "__main__":
+    Argues = namedtuple('Argues', 'yaml')
+    args_ = Argues('multiprocessing_wordpiece_encoder.yml')  # train_sl
+    LOGGER.info(args_)
+    yaml_file = os.path.join(os.path.dirname(__file__), args_.yaml)
+    LOGGER.info('Load arguments in {}'.format(yaml_file))
+    args = load_yaml(yaml_file)
+    LOGGER.info(args)
 
-    parser = argparse.ArgumentParser()
-    # parser.add_argument(
-    #     "--encoder-json",
-    #     help='path to encoder.json',
-    # )
-    # parser.add_argument(
-    #     "--vocab-bpe",
-    #     type=str,
-    #     help='path to vocab.bpe',
-    # )
-    parser.add_argument(
-        "--vocab-size",
-        type=int,
-        default=50000,
-        help='path to vocab.bpe',
-    )
-    parser.add_argument(
-        "--format",
-        type=str,
-        help='path to vocab.bpe',
-    )
+    #  insert special tokens
+    if args['preprocess']['insert']:
+        for i, input_file in enumerate(args['preprocess']['inputs']):
+            if 'docstring' not in input_file:   # only for code, not for docstrings.
+                insert_sep_token(input_file, INSERTED_SUFFIX)
+                args['preprocess']['inputs'][i] = input_file + INSERTED_SUFFIX
+                # args['preprocess']['inputs'] = [input + INSERTED_SUFFIX for input in args['preprocess']['inputs']]
 
-    parser.add_argument(
-        "--model-prefix",
-        type=str,
-        help='path to vocab.bpe',
-    )
-    parser.add_argument(
-        "--inputs",
-        nargs="+",
-        default=['-'],
-        help="input files to filter/encode",
-    )
-    parser.add_argument(
-        "--outputs",
-        nargs="+",
-        default=['-'],
-        help="path to save encoded outputs",
-    )
-    parser.add_argument(
-        "--keep-empty",
-        action="store_true",
-        help="keep empty lines",
-    )
-    parser.add_argument(
-        "--insert",
-        action="store_true",
-        help="keep empty lines",
-    )
-    parser.add_argument("--workers", type=int, default=20)
-    args = parser.parse_args()
+    # build model
+    if args['preprocess']['train_model']:
+    # for input_file in args['preprocess']['inputs']:
 
-    # sp = spm.SentencePieceProcessor()
-    # sp.Load('{}.model'.format(args.model_prefix))
-    # s= "13 23 24 6 3 21 237 12 203 4 204 11 5 6 7 3 537 4 204 10 2707 4 204 12 203 4 204 8 207 11 5 6 7 3 2063 4 204 12 203 4 204 11 5 6 7 3 299 5 6 7 3 528 5 6 7 3 2063 4 204 12 1069 4 204 11 5 6 7 3 15 22"
-    # txt = sp.decode_ids([int(i) for i in s.split(' ')])
-    # print (txt)
-    # sys.exit()
-
-
-    input_file = args.inputs[0]
-
-    if args.insert:
-        insert_sep_token(input_file)
-    if 'train' in input_file:  # only build wordpiece model on train files
-        build_model(input_file + '_inserted', args.model_prefix, args.vocab_size)
-        args.inputs = [input + '_inserted' for input in args.inputs]
-    # build_model(input_file, args.model_prefix, args.vocab_size)
+        # if 'train' in input_file:  # only build wordpiece model on train files
+        build_model(','.join(args['preprocess']['inputs']), args['preprocess']['model_prefix'], args['preprocess']['vocab_size'])
 
     main(args)
--- dataset/codesearchnet/summarization/preprocess_hicodebert_sm.py
+++ dataset/codesearchnet/summarization/preprocess_hicodebert_sm.py
@@ -15,7 +15,6 @@ from ncc.data.binarizer import Binarizer
 import os
 import sys
 from collections import namedtuple
-import torch
 from ncc import tasks
 from ncc.utils.util_file import load_yaml
 from ncc.data import Dictionary
@@ -23,28 +22,10 @@ from ncc.utils import (
     utils, tokenizer
 )
 from ncc import LOGGER
-from typing import Dict, List
-import ujson
-import re
-S_SEP = '<S_SEP>'
-CLS = '<CLS>'
-# S_MASK = '<MASK>'
-# from dataset.codesearchnet.summarization.preprocess_helper import insert_sep_token
+from typing import Dict
 
-# try:
-#     from .preprocess_helper import insert_sep_tokens
-# except:
-#     from dataset.codesearchnet.summarization.preprocess_helper import insert_sep_tokens
 
-# train_path is for build dictionary. Currently, we only insert <S_SEP> to code modality
 def train_path(args, lang):
-    # if lang in args['preprocess']['source_lang']:
-    # if args['preprocess']['inserted']:
-    #     if lang == 'code':
-    #         return "{}_inserted{}".format(args['preprocess']['trainpref'], ("." + lang) if lang else "")
-    #     else:
-    #         return "{}{}".format(args['preprocess']['trainpref'], ("." + lang) if lang else "")
-    # else:
     return "{}{}".format(args['preprocess']['trainpref'], ("." + lang) if lang else "")
 
 
@@ -74,92 +55,40 @@ def build_dictionary(args, task, filenames, src=False, tgt=False):
         padding_factor=args['preprocess']['padding_factor'],
     )
 
-# 这个名字没取好，经常很难理解，而且save操作也不应该放在这里，所以就不抽象出来了
-# def load_dict(args: Dict, task, modality: str, overwrite: bool):
-#     """load dict from (default) dictionary file path. if not exit, load from raw data and save it at default path"""
-#     dict_filenames = dict_path(args, modality)
-#     all_file_exit = all([os.path.exists(filename) for filename in dict_filenames])
-#     if all_file_exit and (not overwrite):
-#         LOGGER.info('Dict({}) exists and overwrite=False, skip this.'.format(dict_filenames))
-#         # dicts = [
-#         #     Dictionary.load(filename)
-#         #     for filename in dict_filenames
-#         # ]
-#         dicts = task.load_dictionaries(dict_filenames)
-#         if len(dicts) == 1:
-#             dicts = dicts[0]
-#     else:
-#         # update dict from data
-#         dicts = build_dictionary(args, task, modality, [train_path(args, modality)], src=True)
-#
-#     return dicts
-
 
 def build_vocab_dict(args: Dict, overwrite: bool = False):
     """Build vocabulary (dictionary) for source and target domain"""
     LOGGER.info('Build vocabularies...')
     task = tasks.get_task(args['preprocess']['task'])
-    # src_dicts = OrderedDict()
     assert args['preprocess']['trainpref'], RuntimeError('Build vocabularies from train dataset, but it is null.')
     target = not args['preprocess']['only_source']
 
     # for joined dictionary
     if args['preprocess']['joined_dictionary']:
-        # for modality in args['preprocess']['source_lang']:
-        # src_dicts[modality] = load_dict(args, task, modality, overwrite)
-        # dict_filenames = dict_path(args, modality)
-        # all_file_exit = all([os.path.exists(filename) for filename in dict_filenames])
-        # if all_file_exit and (not overwrite):
-        # dict_filename = dict_path(args, args['preprocess']['source_lang'])
-        # if dict_filename and (not overwrite):
-        #     LOGGER.info('Dict({}) exists and overwrite=False, skip this.'.format(dict_filename))
         if args['preprocess']['srcdict']:
             src_dict = task.load_dictionary(args['preprocess']['srcdict'])
-            # if len(dicts) == 1:
-            #     dicts = dicts[0]
         else:
             # build dictionary from source_lang and target_lang
             src_dict = build_dictionary(args, task, {train_path(args, lang) for lang in [args['preprocess']['source_lang'], args['preprocess']['target_lang']]}, src=True)
-
-        # the tgt_dict equals
         tgt_dict = src_dict
-        # tgt_dict = src_dicts.values()[-1]
 
     else:
-        # for modality in args['preprocess']['source_lang']:
-        # src_dicts[modality] = load_dict(args, task, modality, overwrite)
-        # dict_filename = dict_path(args, args['preprocess']['source_lang'])
-        # all_file_exit = all([os.path.exists(filename) for filename in dict_filenames])
-        # if all_file_exit and (not overwrite):
-        # if dict_filename and (not overwrite):
-        #     LOGGER.info('Dict({}) exists and overwrite=False, skip this.'.format(dict_filename))
         if args['preprocess']['srcdict']:
             src_dict = task.load_dictionary(args['preprocess']['srcdict'])
-            # if len(dicts) == 1:
-            #     dicts = dicts[0]
         else:
             # update dict from data
             src_dict = build_dictionary(args, task, train_path(args, args['preprocess']['source_lang']), src=True)
 
         if target:
-            # tgt_dict = load_dict(args, task, args['preprocess']['target_lang'], overwrite)
             tgt_dict = build_dictionary(args, task, train_path(args, args['preprocess']['target_lang']), src=True)
         else:
             tgt_dict = None
 
     # save src dict
-    # for modality, src_dict in src_dicts.items():
-    #     dict_filename = dict_path(args, modality)
-    #     LOGGER.info('Save dict(s) for {} in {}.'.format(modality, dict_filename))
-    #     # if isinstance(dicts, Dictionary) and len(dict_filenames) == 1:
-    #     #     dicts.save(dict_filenames[0])
-    #     # else:
-    #     #     for dict_filename, dict in zip(dict_filenames, dicts):
-    #     #         dict.save(dict_filename)
-    #     src_dict.save(dict_filename)
     src_dict.save(dict_path(args, args['preprocess']['source_lang']))
     # save tgt dict
-    tgt_dict.save(dict_path(args, args['preprocess']['target_lang']))
+    if target and tgt_dict is not None:
+        tgt_dict.save(dict_path(args, args['preprocess']['target_lang']))
 
     return src_dict, tgt_dict
 
@@ -173,15 +102,6 @@ def dest_path(args, prefix, lang):
 
 
 def dataset_dest_prefix(args, output_prefix, lang):
-    # base = "{}/{}".format(args['preprocess']['destdir'], output_prefix)
-    # if lang is not None:
-    #     lang_part = ".{}-{}.{}".format(args['preprocess']['source_lang'], args['preprocess']['target_lang'], lang)
-    # elif args['preprocess']['only_source']:
-    #     lang_part = ""
-    # else:
-    #     lang_part = ".{}-{}".format(args['preprocess']['source_lang'], args['preprocess']['target_lang'])
-    #
-    # return "{}{}".format(base, lang_part)
     """dataset file name without extension"""
     dest_file = os.path.join(args['preprocess']['destdir'], '.'.join([output_prefix, lang]))
     return dest_file
@@ -218,11 +138,9 @@ def make_binary_dataset(args, vocab, input_prefix, output_prefix, lang, num_work
         replaced.update(worker_result["replaced"])
         n_seq_tok[0] += worker_result["nseq"]
         n_seq_tok[1] += worker_result["ntok"]
-    # input_prefix = input_prefix + '_inserted' if inserted else input_prefix
     input_file = "{}{}".format(
         input_prefix, ("." + lang) if lang is not None else ""
     )
-    # input_file = os.path.join(input_prefix, 'code', 'train', 'train_all.txt', )
     offsets = Binarizer.find_offsets(input_file, num_workers)
     pool = None
     if num_workers > 1:
@@ -247,8 +165,13 @@ def make_binary_dataset(args, vocab, input_prefix, output_prefix, lang, num_work
     ds = indexed_dataset.make_builder(dataset_dest_file(args, output_prefix, lang, "bin"),
                                       impl=args['preprocess']['dataset_impl'], vocab_size=len(vocab))
     merge_result(
+        # Binarizer.binarize(
+        #     input_file, vocab, lambda t: ds.add_item(t),
+        #     offset=0, end=offsets[1]
+        # )
         Binarizer.binarize(
             input_file, vocab, lambda t: ds.add_item(t),
+            tokenize=tokenizer.CSN_tokenizer('code'),
             offset=0, end=offsets[1]
         )
     )
@@ -288,9 +211,6 @@ def make_dataset(args, vocab, input_prefix, output_prefix, lang, num_workers=1):
         else:
             shutil.copyfile(file_name(input_prefix, lang), output_text_file)
     else:
-        # if lang == 'docstring':
-        #     make_binary_dataset(args, vocab, input_prefix, output_prefix, lang, num_workers)
-        # else:
         make_binary_dataset(args, vocab, input_prefix, output_prefix, lang, num_workers)
 
 
@@ -309,31 +229,15 @@ def make_all(args, lang, vocab):
 
 
 def build_dataset(args: Dict, src_dict: Dictionary, tgt_dict: Dictionary):
-    """build dataset for modal"""
-    # for modality, src_dict in src_dict.items():
-    # LOGGER.info('Building dataset for {}'.format(modality))
-    # if modality == 'path':
-    #     make_all(args, modality, src_dict)
-    # elif modality == 'code':
-    #     make_all(args, modality, src_dict)
-    # else:
     make_all(args, args['preprocess']['source_lang'], src_dict)
     target = not args['preprocess']['only_source']
     if target:
         make_all(args, args['preprocess']['target_lang'], tgt_dict)
 
 
-
-
 def main(args):
     LOGGER.info('mkdir for {} task'.format(args['preprocess']['task']))
     os.makedirs(args['preprocess']['destdir'], exist_ok=True)
-    # 0. insert special tokens for the code modality
-    # if args['preprocess']['inserted']:
-    #     insert_sep_token(args, args['preprocess']['trainpref'], 'train_inserted', 'code')
-    #     insert_sep_token(args, args['preprocess']['validpref'], 'valid_inserted', 'code')
-    #     insert_sep_token(args, args['preprocess']['testpref'], 'test_inserted', 'code')
-
     # 1. build vocabulary
     src_dict, tgt_dict = build_vocab_dict(args, overwrite=True)
     # 2. build dataset
