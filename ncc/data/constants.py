9ce468b2f28f01f620b0d6748efa28b3085bb7aa
whatsmyname <yang.he-3@student.uts.edu.au>
2020-05-16 15:37:16 +1000

    Update constants.py

--- ncc/data/constants.py
+++ ncc/data/constants.py
@@ -6,19 +6,20 @@ PAD = "<pad>"
 EOS = "</s>"
 UNK = "<unk>"
 BOS = "<s>"
-# for path
-H_SEP = '<H_SEP>'
-T_SEP = '<T_SEP>'
-P_SEP = '<P_SEP>'
-PATH_SEPS = [H_SEP, T_SEP, P_SEP]
+
 # for bert
 S_SEP = '<S_SEP>'
 CLS = '<CLS>'
 SNT_SEPS = [S_SEP, CLS]
 
+# for path
+H_SEP = '<H_SEP>'
+T_SEP = '<T_SEP>'
+PATH_SEPS = [H_SEP, T_SEP, S_SEP]
+
 __all__ = [
     'MODES',
     'PAD', 'EOS', 'UNK', 'BOS',
-    'H_SEP', 'T_SEP', 'P_SEP', 'PATH_SEPS',
+    'H_SEP', 'T_SEP', 'PATH_SEPS',
     'S_SEP', 'CLS', 'SNT_SEPS',
 ]
