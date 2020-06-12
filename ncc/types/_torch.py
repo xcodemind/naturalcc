64920d312c22530f87bb0fa59644c1d86590ba10
whatsmyname <yang.he-3@student.uts.edu.au>
2020-06-12 13:29:35 +1000

    Update _torch.py

--- ncc/types/_torch.py
+++ ncc/types/_torch.py
@@ -3,21 +3,33 @@
 from typing import (
     Optional, Union
 )
-from torch import (
-    Tensor,
-    LongTensor,
-    FloatTensor,
-    BoolTensor,
-)
+
 from torch.optim.optimizer import Optimizer
 from torch.utils.data import Dataset, DataLoader
 from torch.nn import Module
 
 # torch Tensor
-Tensor_t = Optional[Tensor]
-LTensor_t = LongTensor
-FTensor_t = FloatTensor
-BTensor_t = Union[BoolTensor, FloatTensor, None]  # for mask
+# torch version conflict
+try:
+    from torch import (
+        Tensor,
+        LongTensor,
+        FloatTensor,
+        BoolTensor,
+    )
+
+    Tensor_t = Optional[Tensor]
+    LTensor_t = LongTensor
+    FTensor_t = FloatTensor
+    BTensor_t = Union[BoolTensor, FloatTensor, None]  # for mask
+except:
+    from torch import Tensor
+
+    Tensor_t = Optional[Tensor]
+    LTensor_t = Tensor
+    FTensor_t = Tensor
+    BTensor_t = Optional[Tensor]  # for mask
+
 ThOptimizer_t = Optimizer
 ThDataset_t = Dataset
 ThDataLoader_t = DataLoader
