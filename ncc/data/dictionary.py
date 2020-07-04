1501a51cc5341357c1d6398bd7b32ed5a9e14617
whatsmyname <yang.he-3@student.uts.edu.au>
2020-07-04 19:31:51 +1000

    Update dictionary.py
    add json format load/save

--- ncc/data/dictionary.py
+++ ncc/data/dictionary.py
@@ -6,7 +6,7 @@
 from typing import *
 
 import os
-import ujson
+import json
 from collections import Counter
 from multiprocessing import Pool
 import itertools
@@ -231,13 +231,76 @@ class Dictionary(object):
         lines = f.readlines()
         indices_start_line = self._load_meta(lines)
 
-        for line in lines[indices_start_line:]:
+        for idx, line in enumerate(lines[indices_start_line:]):
             try:
                 line, field = line.rstrip().rsplit(" ", 1)
                 if field == "#fairseq:overwrite":
                     overwrite = True
                     line, field = line.rsplit(" ", 1)
                 else:
+                    overwrite = False
+                count = int(field)
+                word = line
+                if word in self and not overwrite:
+                    raise RuntimeError(
+                        "Duplicate word found when loading Dictionary: '{}'. "
+                        "Duplicate words can overwrite earlier ones by adding the "
+                        "#fairseq:overwrite flag at the end of the corresponding row "
+                        "in the dictionary file. If using the Camembert model, please "
+                        "download an updated copy of the model file."
+                            .format(word)
+                    )
+                self.add_symbol(word, n=count, overwrite=overwrite)
+            except ValueError:
+                raise ValueError(
+                    "Incorrect dictionary format, expected '<token> <cnt> [flags]'"
+                )
+
+    @classmethod
+    def load_json(cls, f):
+        """Loads the dictionary from a text file with the format:
+        ```
+        <symbol0> <count0>
+        <symbol1> <count1>
+        ...
+        ```
+        """
+        if str.endswith(f, '.txt'):
+            f = f.replace('.txt', '.json')
+        d = cls()
+        d.add_from_json_file(f)
+        return d
+
+    def add_from_json_file(self, f):
+        """
+        Loads a pre-existing dictionary from a text file and adds its symbols
+        to this instance.
+        """
+        if isinstance(f, str):
+            try:
+                with PathManager.open(f, "r", encoding="utf-8") as fd:
+                    self.add_from_json_file(fd)
+            except FileNotFoundError as fnfe:
+                raise fnfe
+            except UnicodeError:
+                raise Exception(
+                    "Incorrect encoding detected in {}, please "
+                    "rebuild the dataset".format(f)
+                )
+            return
+
+        lines = f.readlines()
+        indices_start_line = self._load_meta(lines)
+
+        for line in lines[indices_start_line:]:
+            try:
+                raw_line = json.loads(line.rstrip())
+                line, field = raw_line[:-1], raw_line[-1]
+                if field == "#fairseq:overwrite":
+                    overwrite = True
+                    line, field = line[:-1], line[-1]
+                else:
+                    line = line[0]
                     overwrite = False
                 count = int(field)
                 word = line
@@ -264,6 +327,14 @@ class Dictionary(object):
         for k, v in kv_iterator:
             print("{} {}".format(k, v), file=f)
 
+    def _save_json(self, f, kv_iterator):
+        if isinstance(f, str):
+            PathManager.mkdirs(os.path.dirname(f))
+            with PathManager.open(f, "w", encoding="utf-8") as fd:
+                return self.save_json(fd)
+        for k, v in kv_iterator:
+            print(json.dumps([k, v]), file=f)
+
     def _get_meta(self):
         return [], []
 
@@ -274,6 +345,19 @@ class Dictionary(object):
         """Stores dictionary into a text file"""
         ex_keys, ex_vals = self._get_meta()
         self._save(
+            f,
+            zip(
+                ex_keys + self.symbols[self.nspecial:],
+                ex_vals + self.count[self.nspecial:],
+            ),
+        )
+
+    def save_json(self, f):
+        """Stores dictionary into a text file"""
+        if isinstance(f, str) and str.endswith(f, '.txt'):
+            f = f.replace('.txt', '.json')
+        ex_keys, ex_vals = self._get_meta()
+        self._save_json(
             f,
             zip(
                 ex_keys + self.symbols[self.nspecial:],
