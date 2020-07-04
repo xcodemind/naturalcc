ae7022f042305b85d2e6d70eba69b18e071e0480
Yao <wanyao1992@gmail.com>
2020-07-04 22:15:08 +0800

    not shuffle seqrnndataset for debug

--- ncc/data/completion/seqrnn_dataset.py
+++ ncc/data/completion/seqrnn_dataset.py
@@ -224,8 +224,8 @@ class SeqRNNDataset(FairseqDataset):
         else:
             indices = np.arange(len(self))
         if self.tgt_sizes is not None:
-            indices = indices[np.argsort(self.tgt_sizes[indices], kind='mergesort')]
-        return indices[np.argsort(self.src_sizes[indices], kind='mergesort')]
+            indices = indices #[np.argsort(self.tgt_sizes[indices], kind='mergesort')]
+        return indices #[np.argsort(self.src_sizes[indices], kind='mergesort')]
 
     @property
     def supports_prefetch(self):
