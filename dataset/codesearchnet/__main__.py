70fef4560ae54b882266224b0ee2d605868ea1a8
whatsmyname <yang.he-3@student.uts.edu.au>
2020-07-07 18:36:23 +1000

    Update __main__.py

--- dataset/codesearchnet/__main__.py
+++ dataset/codesearchnet/__main__.py
@@ -16,11 +16,11 @@ def main():
     lngs = ['ruby']
     # flatten_attrs = ['code', 'docstring']
     flatten_attrs = ['code_tokens', 'docstring_tokens']
-    tree_attrs = ['path']
-    all_attrs = flatten_attrs + tree_attrs
+    # tree_attrs = ['path']
+    # all_attrs = flatten_attrs + tree_attrs
     # flatten raw files separately
-    # dataset.flatten_data_all(lngs, save_attrs=flatten_attrs, overwrite=True)
-    # # # parse raw_ast into other new tree modalities
+    dataset.flatten_data_all(lngs, save_attrs=flatten_attrs, overwrite=True)
+    # parse raw_ast into other new tree modalities
     # dataset.parse_new_tree_modalities_all(lngs, modalities=tree_attrs, overwrite=True)
     # use cat command to merge files
     dataset.merge_attr_files(lngs, attrs=flatten_attrs)
