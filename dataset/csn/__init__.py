ee7e2107ce280e335153b4298060a853d406e118
whatsmyname <873715822@qq.com>
2020-08-02 22:03:35 +1000

    Create __init__.py

---
+++ dataset/csn/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+DATASET_NAME = 'code_search_net'
+RAW_DATA_DIR = '~/.ncc/{}/raw'.format(DATASET_NAME)
+LIBS_DIR = '~/.ncc/{}/libs'.format(DATASET_NAME)
+FLATTEN_DIR = '~/.ncc/{}/flatten'.format(DATASET_NAME)
+
+LANGUAGES = ['ruby', 'python', 'java', 'go', 'php', 'javascript']
+MODES = ['train', 'valid', 'test']
+
+MEANINGLESS_TOKENS = set(['(', ')', '[', ']', '{', '}', ';', '@', '#', ':', '()', '<>', '{}'])
+COMMENT_END_TOKENS = set(['{', '[', '('])
+
+SBT_PARENTHESES = ['(_SBT', ')_SBT']
+
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)-15s] %(levelname)7s >> %(message)s (%(filename)s:%(lineno)d, %(funcName)s())',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+LOGGER = logging.getLogger(__name__)
