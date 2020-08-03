a5788dad1fa1ac66811214b29c975783daab97c0
whatsmyname <873715822@qq.com>
2020-08-03 19:06:39 +1000

    register code2seq

--- ncc/models/summarization/__init__.py
+++ ncc/models/summarization/__init__.py
@@ -1,8 +1,10 @@
 from .lstm import LSTMModel
 from .mm2seq import MM2SeqModel
+from .code2seq import Code2Seq
 from .hi_transformer_summarization import HiTransformerSummarizationModel
 from .transformer_summarization import TransformerModel
 from .transformer_summarization_ft import TransformerFtModel
+
 __all__ = [
-    'LSTMModel', 'MM2SeqModel', 'HiTransformerSummarizationModel', 'TransformerModel', 'TransformerFtModel'
+    'LSTMModel', 'MM2SeqModel', 'HiTransformerSummarizationModel', 'TransformerModel', 'TransformerFtModel', 'Code2Seq'
 ]
