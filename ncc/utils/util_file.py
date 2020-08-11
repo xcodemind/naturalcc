32f9be98667b856fc76606ff128e0013e56a423a
whatsmyname <873715822@qq.com>
2020-08-11 23:52:29 +1000

    to make load yaml more readable

--- ncc/utils/util_file.py
+++ ncc/utils/util_file.py
@@ -18,47 +18,20 @@ def load_yaml(yaml_file: str) -> Dict:
     read yaml file
     :param yaml_file:
     '''
+
+    def expanduser_for_dict(dictionary):
+        for key, value in dictionary.items():
+            if isinstance(value, dict):
+                expanduser_for_dict(value)
+
+            if isinstance(value, str) and value.startswith('~/'):
+                dictionary[key] = os.path.expanduser(value)
+            elif isinstance(value, list):
+                for i, val in enumerate(value):
+                    if val.startswith('~/'):
+                        value[i] = os.path.expanduser(val)
+
     with open(yaml_file, 'r', encoding='utf-8') as reader:
         args = yaml.safe_load(reader)
-    # TODO: To be more elegant
-    if 'preprocess' in args:
-        # os.path.expanduser： ~/.ncc -> /home/user/.ncc
-        for key, value in args['preprocess'].items():
-            if isinstance(value, str) and value.startswith('~/'):
-                args['preprocess'][key] = os.path.expanduser(value)
-            if isinstance(value, list):
-                for i, val in enumerate(value):
-                    if val.startswith('~/'):
-                        value[i] = os.path.expanduser(val)
-
-    if 'task' in args:
-        # os.path.expanduser： ~/.ncc -> /home/user/.ncc
-        for key, value in args['task'].items():
-            if isinstance(value, str) and value.startswith('~/'):
-                args['task'][key] = os.path.expanduser(value)
-            if isinstance(value, list):
-                for i, val in enumerate(value):
-                    if val.startswith('~/'):
-                        value[i] = os.path.expanduser(val)
-
-    if 'checkpoint' in args:
-        # os.path.expanduser： ~/.ncc -> /home/user/.ncc
-        for key, value in args['checkpoint'].items():
-            if isinstance(value, str) and value.startswith('~/'):
-                args['checkpoint'][key] = os.path.expanduser(value)
-            if isinstance(value, list):
-                for i, val in enumerate(value):
-                    if val.startswith('~/'):
-                        value[i] = os.path.expanduser(val)
-
-    if 'eval' in args:
-        # os.path.expanduser： ~/.ncc -> /home/user/.ncc
-        for key, value in args['eval'].items():
-            if isinstance(value, str) and value.startswith('~/'):
-                args['eval'][key] = os.path.expanduser(value)
-            if isinstance(value, list):
-                for i, val in enumerate(value):
-                    if val.startswith('~/'):
-                        value[i] = os.path.expanduser(val)
-
+    expanduser_for_dict(args)
     return args
