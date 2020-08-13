3886409237443572e6c6abe771c2c0a800a43ce8
yang <873715822@qq.com>
2020-08-14 03:57:28 +1000

    fix a bug of clip_grad_norm_

--- ncc/utils/utils.py
+++ ncc/utils/utils.py
@@ -506,19 +506,16 @@ def item(tensor):
     return tensor
 
 
-def clip_grad_norm_(params, max_norm) -> torch.Tensor:
+def clip_grad_norm_(params, max_norm, aggregate_norm_fn=None) -> torch.Tensor:
     if isinstance(params, torch.Tensor):
         params = [params]
+    params = list(params)
     grads = [p.grad.detach() for p in filter(lambda p: p.grad is not None, params)]
     if len(grads) == 0:
-        return params[0].new_tensor(0.)
-    total_norm = torch.norm(torch.stack([torch.norm(g) for g in grads]))
-    if max_norm > 0:
-        max_norm = float(max_norm)
-        clip_coef = (max_norm / (total_norm + 1e-6)).clamp_(max=1)
-        for g in grads:
-            g.mul_(clip_coef)
-    return total_norm
+        if len(params) > 0:
+            return params[0].new_tensor(0.)
+        else:
+            return torch.tensor(0.)
 
 
 def fill_with_neg_inf(t):
