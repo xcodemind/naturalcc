d85a232039f23621d2f79c5557db2f666ca02515
yang <873715822@qq.com>
2020-08-25 01:25:11 +1000

    Update run.sh

--- run/summarization/finetune/seq2seq/run.sh
+++ run/summarization/finetune/seq2seq/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # pretrain
-nohup python -m run.summarization.finetune.seq2seq.pretrain > run/summarization/finetune/finetune/seq2seq/pretrain.log 2>&1
+nohup python -m run.summarization.finetune.seq2seq.pretrain > run/summarization/finetune/seq2seq/pretrain.csn.log 2>&1
 # finetune
-nohup python -m run.summarization.finetune.seq2seq.finetune > run/summarization/finetune/finetune/seq2seq/finetune.log 2>&1
+nohup python -m run.summarization.finetune.seq2seq.finetune > run/summarization/finetune/seq2seq/finetune.csn2csharp.log 2>&1
