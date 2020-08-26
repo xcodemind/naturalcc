42c169f108543e85bf59f5f26fe5bab087e6d0b8
yang <873715822@qq.com>
2020-08-26 13:04:41 +1000

    finalize dataset to run experiments

--- dataset/csn_msra/summarization/run.sh
+++ dataset/csn_msra/summarization/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# finalize dataset on each language's dictionaries
 nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs_100k -l ruby > log  2>&1 &
 nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs_100k -l go > go.log  2>&1 &
 nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs_100k -l python > python.log  2>&1 &
@@ -8,4 +9,5 @@ nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs_100k -l jav
 nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs_100k -l javascript > javascript.log  2>&1 &
 nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs_100k -l csharp > csharp.log  2>&1 &
 
+# finalize dataset on a shared dictionary of CSN and C# dataset
 nohup python -m dataset.csn_msra.summarization.preprocess_multilingual_100k > all.log  2>&1 &
