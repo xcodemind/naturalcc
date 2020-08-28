5c6ae24b12a358ace54a424dbc312fc28323b92d
yang <873715822@qq.com>
2020-08-29 02:22:15 +1000

    java_hu and csn dataset generation commands

--- projects/icse.sh
+++ projects/icse.sh
@@ -32,3 +32,29 @@ python -m dataset.python_wan.summarization.preprocess_multiattrs -p 0.01
 
 #csn => Python_wan, shared dict
 python -m dataset.python_wan.summarization.preprocess_multilingual
+
+## Java_hu
+#bash dataset/java_hu/download.sh
+#python -m dataset.java_hu.build
+#python -m dataset.java_hu.src_code
+#python -m dataset.java_hu.flatten
+#python -m dataset.csn_msra.feature_extract -l java -f ~/.ncc/java_hu/flatten -r ~/.ncc/java_hu/refine -s ~/.ncc/java_hu/libs
+#python -m dataset.csn_msra.filter -l java -r ~/.ncc/java_hu/refine -f ~/.ncc/java_hu/filter
+#python -m dataset.java_hu.portion
+#
+##binarize for debug and fast load
+##baseline
+#python -m dataset.java_hu.summarization.preprocess_multiattrs -p 1.0
+#python -m dataset.java_hu.summarization.preprocess_multiattrs -p 0.8
+#python -m dataset.java_hu.summarization.preprocess_multiattrs -p 0.7
+#python -m dataset.java_hu.summarization.preprocess_multiattrs -p 0.6
+#python -m dataset.java_hu.summarization.preprocess_multiattrs -p 0.5
+#python -m dataset.java_hu.summarization.preprocess_multiattrs -p 0.4
+#python -m dataset.java_hu.summarization.preprocess_multiattrs -p 0.3
+#python -m dataset.java_hu.summarization.preprocess_multiattrs -p 0.2
+#python -m dataset.java_hu.summarization.preprocess_multiattrs -p 0.1
+#python -m dataset.java_hu.summarization.preprocess_multiattrs -p 0.05
+#python -m dataset.java_hu.summarization.preprocess_multiattrs -p 0.01
+#
+##csn => Java_hu, shared dict
+#python -m dataset.java_hu.summarization.preprocess_multilingual
