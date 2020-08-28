d78edee45a151b5d188a4e10ff57673a31f50db1
yang <873715822@qq.com>
2020-08-29 02:04:56 +1000

    csn msra

--- dataset/csn_msra/summarization/run.sh
+++ dataset/csn_msra/summarization/run.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+
 # finalize dataset on each language's dictionaries
-nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs_100k -l ruby > log  2>&1 &
-nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs_100k -l go > go.log  2>&1 &
-nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs_100k -l python > python.log  2>&1 &
-nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs_100k -l php > php.log  2>&1 &
-nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs_100k -l java > java.log  2>&1 &
-nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs_100k -l javascript > javascript.log  2>&1 &
-nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs_100k -l csharp > csharp.log  2>&1 &
+nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs -l ruby > ruby.log  2>&1
+nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs -l go > go.log  2>&1
+nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs -l python > python.log  2>&1
+nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs -l php > php.log  2>&1
+nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs -l java > java.log  2>&1
+nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs -l javascript > javascript.log  2>&1
+nohup python -m dataset.csn_msra.summarization.preprocess_multiattrs -l csharp > csharp.log  2>&1
 
 # finalize dataset on a shared dictionary of CSN and C# dataset
-nohup python -m dataset.csn_msra.summarization.preprocess_multilingual_100k > all.log  2>&1 &
+nohup python -m dataset.csn_msra.summarization.preprocess_multilingual > all.log  2>&1
