c451eb6e01b7c233f109bd1909e64c1098c6d05e
yang <873715822@qq.com>
2020-08-29 02:12:59 +1000

    travtrans

--- run/summarization/travtrans/run.sh
+++ run/summarization/travtrans/run.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-nohup python -m run.summarization.transformer_ast.train -l ruby > run/summarization/transformer_ast/ruby.log 2>&1
-nohup python -m run.summarization.transformer_ast.train -l python > run/summarization/transformer_ast/python.log 2>&1
-nohup python -m run.summarization.transformer_ast.train -l php > run/summarization/transformer_ast/php.log 2>&1
-nohup python -m run.summarization.transformer_ast.train -l java > run/summarization/transformer_ast/java.log 2>&1
-nohup python -m run.summarization.transformer_ast.train -l javascript > run/summarization/transformer_ast/javascript.log 2>&1
-nohup python -m run.summarization.transformer_ast.train -l go > run/summarization/transformer_ast/go.log 2>&1
-nohup python -m run.summarization.transformer_ast.train -l csharp > run/summarization/transformer_ast/csharp.log 2>&1
+nohup python -m run.summarization.travtrans.train -l ruby > run/summarization/travtrans/ruby.log 2>&1
+nohup python -m run.summarization.travtrans.train -l python > run/summarization/travtrans/python.log 2>&1
+nohup python -m run.summarization.travtrans.train -l php > run/summarization/travtrans/php.log 2>&1
+nohup python -m run.summarization.travtrans.train -l java > run/summarization/travtrans/java.log 2>&1
+nohup python -m run.summarization.travtrans.train -l javascript > run/summarization/travtrans/javascript.log 2>&1
+nohup python -m run.summarization.travtrans.train -l go > run/summarization/travtrans/go.log 2>&1
+nohup python -m run.summarization.travtrans.train -l csharp > run/summarization/travtrans/csharp.log 2>&1
+
+
+nohup python -m run.summarization.travtrans.eval -l ruby > run/summarization/travtrans/ruby.eval.log 2>&1
+nohup python -m run.summarization.travtrans.eval -l php > run/summarization/travtrans/php.eval.log 2>&1
+nohup python -m run.summarization.travtrans.eval -l python > run/summarization/travtrans/python.eval.log 2>&1
+nohup python -m run.summarization.travtrans.eval -l java > run/summarization/travtrans/java.eval.log 2>&1
+nohup python -m run.summarization.travtrans.eval -l javascript > run/summarization/travtrans/javascript.eval.log 2>&1
+nohup python -m run.summarization.travtrans.eval -l go > run/summarization/travtrans/go.eval.log 2>&1
+nohup python -m run.summarization.travtrans.eval -l csharp > run/summarization/travtrans/csharp.eval.log 2>&1
