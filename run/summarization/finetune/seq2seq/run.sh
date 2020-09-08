0688a0cc944f99cf713a8163ca98c1885fb5069f
yang <873715822@qq.com>
2020-09-08 21:16:37 +1000

    delete finetune on seq2seq

--- run/summarization/finetune/seq2seq/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# pretrain
-nohup python -m run.summarization.finetune.seq2seq.pretrain > run/summarization/finetune/seq2seq/pretrain.csn.log 2>&1
-# finetune
-nohup python -m run.summarization.finetune.seq2seq.finetune > run/summarization/finetune/seq2seq/finetune.csn2csharp.log 2>&1
