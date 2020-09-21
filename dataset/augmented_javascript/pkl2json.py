727def28f355fc25a9ada26b4bc6904a7ac7a490
Yang.He <yanghece96@gmail.com>
2020-09-21 11:19:13 +1000

    Add files via upload

---
+++ dataset/augmented_javascript/pkl2json.py
@@ -0,0 +1,28 @@
+import os
+import gzip
+import ujson
+import pickle
+from tqdm import tqdm
+from dataset.augmented_javascript import (
+    RAW_DATA_DIR,
+)
+
+
+def get_file_lines(f):
+    f.seek(0)
+    num = sum(1 for line in f)
+    f.seek(0)
+    return num
+
+
+if __name__ == '__main__':
+    min_alternatives = 2
+
+    with gzip.open(os.path.join(RAW_DATA_DIR, 'javascript_augmented.pickle.gz'), 'rb') as reader, \
+        open(os.path.join(RAW_DATA_DIR, 'javascript_augmented.json'), 'w') as writer:
+        examples = map(list, pickle.load(reader))
+        if min_alternatives:
+            examples = filter(lambda ex: len(ex) >= min_alternatives, examples)
+        examples = list(examples)
+        for exs in tqdm(examples):
+            print(ujson.dumps(exs[0]), file=writer)
