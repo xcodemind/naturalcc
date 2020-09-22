5324ebb4a1128472c8ace76f851eccdf7c92e887
Yao <wanyao1992@gmail.com>
2020-09-23 01:55:24 +0800

    Update cast_type_prediction_data.py

--- dataset/augmented_javascript/cast_type_prediction_data.py
+++ dataset/augmented_javascript/cast_type_prediction_data.py
@@ -10,13 +10,21 @@ output_path = os.path.join(os.path.dirname(RAW_DATA_DIR_TYPE_PREDICTION), 'data-
 os.makedirs(output_path, exist_ok=True)
 
 
+# def cast_file(file_name, mode, src, tgt):
+#     with open(file_name, 'r') as input_file, \
+#         open(os.path.join(output_path, '{}.{}'.format(mode, src)), 'w') as code_file, \
+#         open(os.path.join(output_path, '{}.{}'.format(mode, tgt)), 'w') as type_file:
+#         for line in input_file.readlines():
+#             print(ujson.dumps(line.split('\t')[0], ensure_ascii=False), file=code_file)
+#             print(ujson.dumps(line.split('\t')[1], ensure_ascii=False), end='', file=type_file)
+
 def cast_file(file_name, mode, src, tgt):
     with open(file_name, 'r') as input_file, \
-        open(os.path.join(output_path, '{}.{}'.format(mode, src)), 'w') as code_file, \
-        open(os.path.join(output_path, '{}.{}'.format(mode, tgt)), 'w') as type_file:
+            open(os.path.join(output_path, '{}.{}'.format(mode, src)), 'w') as code_file, \
+            open(os.path.join(output_path, '{}.{}'.format(mode, tgt)), 'w') as type_file:
         for line in input_file.readlines():
-            print(ujson.dumps(line.split('\t')[0], ensure_ascii=False), file=code_file)
-            print(ujson.dumps(line.split('\t')[1], ensure_ascii=False), end='', file=type_file)
+            code_file.write(line.split('\t')[0] + '\n')
+            type_file.write(line.split('\t')[1])
 
 
 if __name__ == '__main__':
