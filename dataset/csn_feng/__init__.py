f981400f68428cce24f0cac4f7f6c5257ea09a3d
yang <873715822@qq.com>
2020-09-25 00:07:27 +1000

    csn_feng retrieval task

--- dataset/csn_feng/__init__.py
+++ dataset/csn_feng/__init__.py
@@ -1,16 +1,29 @@
 # -*- coding: utf-8 -*-
 
 import os
+from dataset import (
+    HOSTNAME, USERNAME, DEFAULT_DIR, LOGGER,
+)
 
 DATASET_NAME = 'code_search_net_feng'
-RAW_DATA_DIR = '~/.ncc/{}/raw'.format(DATASET_NAME)
-LIBS_DIR = '~/.ncc/{}/libs'.format(DATASET_NAME)
-FLATTEN_DIR = '~/.ncc/{}/flatten'.format(DATASET_NAME)
-REFINE_DIR = '~/.ncc/{}/refine'.format(DATASET_NAME)
-FILTER_DIR = '~/.ncc/{}/filter'.format(DATASET_NAME)
+DATASET_DIR = os.path.join(DEFAULT_DIR, DATASET_NAME)
+RAW_DATA_DIR = os.path.join(DATASET_DIR, 'raw')
+LIBS_DIR = os.path.join(DEFAULT_DIR, 'libs')
+FLATTEN_DIR = os.path.join(DATASET_DIR, 'flatten')
+REFINE_DIR = os.path.join(DATASET_DIR, 'refine')
+FILTER_DIR = os.path.join(DATASET_DIR, 'filter')
 
 RAW_DATA_DIR, LIBS_DIR, FLATTEN_DIR, REFINE_DIR, FILTER_DIR = \
     map(os.path.expanduser, (RAW_DATA_DIR, LIBS_DIR, FLATTEN_DIR, REFINE_DIR, FILTER_DIR))
+
+_RETRIEVAL_RAW_DATA_DIR = os.path.join(RAW_DATA_DIR, 'retrieval')
+
+RETRIEVAL_RAW_DATA_DIR = {
+    'train': os.path.join(_RETRIEVAL_RAW_DATA_DIR, 'train_valid'),
+    'valid': os.path.join(_RETRIEVAL_RAW_DATA_DIR, 'train_valid'),
+    'test': _RETRIEVAL_RAW_DATA_DIR,
+}
+RETRIEVAL_SPLITTER = '<CODESPLIT>'
 
 LANGUAGES = ['ruby', 'python', 'java', 'go', 'php', 'javascript']
 MODES = ['train', 'valid', 'test']
@@ -24,11 +37,9 @@ SBT_PARENTHESES = ['(_SBT', ')_SBT']
 # for binary-AST
 NODE_TMP = 'TMP'
 
-import logging
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='[%(asctime)-15s] %(levelname)7s >> %(message)s (%(filename)s:%(lineno)d, %(funcName)s())',
-    datefmt='%Y-%m-%d %H:%M:%S',
+__all__ = (
+    DATASET_NAME,
+    RAW_DATA_DIR, LIBS_DIR, FLATTEN_DIR, REFINE_DIR, FILTER_DIR,
+    LANGUAGES, MODES,
+    RECURSION_DEPTH, PATH_NUM, MAX_SUB_TOKEN_LEN, SBT_PARENTHESES, NODE_TMP,
 )
-LOGGER = logging.getLogger(__name__)
