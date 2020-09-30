91d99ec1cb31302bbf2c54f56d14ba8b8e02acc2
yang <873715822@qq.com>
2020-09-30 12:52:08 +1000

    to generate traversal data for csn dataset

---
+++ dataset/csn/traversal.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#python -m dataset.csn.download
+python -m dataset.csn.flattn
+python -m dataset.csn.feature_extract -a code_tokens docstring_tokens raw_ast ast traversal
+python -m dataset.csn.filter -a code_tokens docstring_tokens traversal
+
+python -m dataset.csn.summarization.preprocess_multiattrs -l ruby
+python -m dataset.csn.summarization.preprocess_multiattrs -l php
+python -m dataset.csn.summarization.preprocess_multiattrs -l java
+python -m dataset.csn.summarization.preprocess_multiattrs -l javascript
+python -m dataset.csn.summarization.preprocess_multiattrs -l python
+python -m dataset.csn.summarization.preprocess_multiattrs -l go
