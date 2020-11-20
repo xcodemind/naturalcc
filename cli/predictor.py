e01f107c9cbd158a8d07107689ffee90b33f587d
Yang.He <yanghece96@gmail.com>
2020-11-21 00:13:56 +1100

    update ssummarization interface

--- cli/predictor.py
+++ cli/predictor.py
@@ -51,28 +51,22 @@ def main(model_path, input):
 
 def cli_main():
     # code summarization
+    model_path = os.path.expanduser("~/.ncc/demo/summarization/seq2seq/python_wan.pt")
+    model_path = os.path.expanduser("~/.ncc/demo/summarization/neural_transformer/python_wan.pt")
     codes = [
         "def positional(max_positional_args):\n\tdef positional_decorator(wrapped):\n\t\t@functools.wraps(wrapped)\n\t\tdef positional_wrapper(*args, **kwargs):\n\t\t\tif (len(args) > max_posi      tional_args):\n\t\t\t\tplural_s = ''\n\t\t\t\tif (max_positional_args != 1):\n\t\t\t\t\tplural_s = 's'\n\t\t\t\tmessage = ('%s()\ttakes\tat\tmost\t%d\tpositional\targument%s\t(%d\tgive      n)' % (wrapped.__name__, max_positional_args, plural_s, len(args)))\n\t\t\t\tif (positional_parameters_enforcement == POSITIONAL_EXCEPTION):\n\t\t\t\t\traise TypeError(message)\n\t\t\t      \telif (positional_parameters_enforcement == POSITIONAL_WARNING):\n\t\t\t\t\tlogger.warning(message)\n\t\t\t\telse:\n\t\t\t\t\tpass\n\t\t\treturn wrapped(*args, **kwargs)\n\t\treturn p      ositional_wrapper\n\tif isinstance(max_positional_args, six.integer_types):\n\t\treturn positional_decorator\n\telse:\n\t\t(args, _, _, defaults) = inspect.getargspec(max_positional_ar      gs)\n\t\treturn positional((len(args) - len(defaults)))(max_positional_args)\n",
         "def getCarveIntersectionFromEdge(edge, vertexes, z):\n\tfirstVertex = vertexes[edge.vertexIndexes[0]]\n\tfirstVertexComplex = firstVertex.dropAxis(2)\n\tsecondVertex = vertexes[edge.v      ertexIndexes[1]]\n\tsecondVertexComplex = secondVertex.dropAxis(2)\n\tzMinusFirst = (z - firstVertex.z)\n\tup = (secondVertex.z - firstVertex.z)\n\treturn (((zMinusFirst * (secondVerte      xComplex - firstVertexComplex)) \/ up) + firstVertexComplex)\n",
         "def MessageEncoder(field_number, is_repeated, is_packed):\n\ttag = TagBytes(field_number, wire_format.WIRETYPE_LENGTH_DELIMITED)\n\tlocal_EncodeVarint = _EncodeVarint\n\tassert (not i      s_packed)\n\tif is_repeated:\n\t\tdef EncodeRepeatedField(write, value):\n\t\t\tfor element in value:\n\t\t\t\twrite(tag)\n\t\t\t\tlocal_EncodeVarint(write, element.ByteSize())\n\t\t\t      \telement._InternalSerialize(write)\n\t\treturn EncodeRepeatedField\n\telse:\n\t\tdef EncodeField(write, value):\n\t\t\twrite(tag)\n\t\t\tlocal_EncodeVarint(write, value.ByteSize())\n\      t\t\treturn value._InternalSerialize(write)\n\t\treturn EncodeField\n",
-        "def service_delete(service_id=None, name=None, profile=None, **connection_args):\n\tkstone = auth(profile, **connection_args)\n\tif name:\n\t\tservice_id = service_get(name=name, prof      ile=profile, **connection_args)[name]['id']\n\tkstone.services.delete(service_id)\n\treturn 'Keystone\tservice\tID\t\"{0}\"\tdeleted'.format(service_id)\n",
-        "def verify(user, password):\n\tdef verify_user(user_name, user_password):\n\t\tif ((user_name == user) and (user_password == password)):\n\t\t\treturn user_name\n\t\treturn False\n\tr      eturn verify_user\n",
     ]
     gts = [
         "a decorator to declare that only the first n arguments my be positional.",
         "get the complex where the carve intersects the edge .",
         "returns an encoder for a message field .",
-        "service delete .",
-        "verifies that the signature matches the message .",
     ]
 
     import argparse
     parser = argparse.ArgumentParser(description="Command Interface")
-    parser.add_argument(
-        "--model", "-m", type=str, help="pytorch model path",
-        # default=os.path.expanduser("~/.ncc/demo/summarization/seq2seq/python_wan.pt")
-        default=os.path.expanduser("~/.ncc/demo/summarization/neural_transformer/python_wan.pt")
-    )
+    parser.add_argument("--model", "-m", type=str, help="pytorch model path", default=model_path)
     parser.add_argument("--input", "-i", type=str, help="model input")
     args = parser.parse_args()
     for code, gt in zip(codes, gts):
