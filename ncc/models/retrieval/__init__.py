1e01593eeb1972c5ef147ebf862650f8eff06143
whatsmyname <yanghece96@gmail.com>
2020-11-26 21:15:19 +1100

    remove some baselines

--- ncc/models/retrieval/__init__.py
+++ ncc/models/retrieval/__init__.py
@@ -1,3 +1,1 @@
 from .nbow import NBOW
-from .conv1d_res import Conv1dRes
-from .rnn import RNN
--- ncc/models/summarization/__init__.py
+++ ncc/models/summarization/__init__.py
@@ -1,22 +1,9 @@
 from .seq2seq import Seq2SeqModel
-from .mm2seq import MM2SeqModel
-from .code2seq import Code2Seq
-from .hi_transformer_summarization import HiTransformerSummarizationModel
 from .transformer import TransformerModel
-# from .transformer_summarization_ft import TransformerFtModel
 from .neural_transformer import NeuralTransformerModel
-from .transformer_from_roberta import TransformerFromRobertaModel
-from .codenn import CodeNNModel
-from .deepcom import DeepComModel
 
 __all__ = [
-    'Seq2SeqModel', 'MM2SeqModel',
-    'HiTransformerSummarizationModel',
+    'Seq2SeqModel',
     'TransformerModel',
-    # 'TransformerFtModel',
     'NeuralTransformerModel',
-    'TransformerFromRobertaModel',
-    'Code2Seq',
-    'CodeNNModel',
-    'DeepComModel',
 ]
