f040e039c4410343c00b84ace0f55a0585781b23
whatsmyname <yanghece96@gmail.com>
2020-11-26 16:37:13 +1100

    make dataset DEFAULT_DIR at ~/.ncc

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -9,12 +9,7 @@ from ncc import LOGGER
 HOSTNAME = socket.gethostname()
 USERNAME = getpass.getuser()
 # register your hostname or username
-if HOSTNAME in ['GS65', 'node14'] or re.match(r'pytorch-.*', HOSTNAME) is not None \
-    or USERNAME in ['hust_xhshi_1']:
-    DEFAULT_DIR = '~/.ncc'
-else:
-    DEFAULT_DIR = '/export/share/jianguo/scodebert/'
-
+DEFAULT_DIR = '~/.ncc'
 DEFAULT_DIR = os.path.expanduser(DEFAULT_DIR)
 LIBS_DIR = os.path.join(os.path.dirname(__file__), 'tree-sitter-libs')
 LOGGER.debug('Host Name: {}; User Name: {}; Default data directory: {}'.format(HOSTNAME, USERNAME, DEFAULT_DIR))
