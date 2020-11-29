822fcde701321c1f55099929c0da00ce99c5469d
whatsmyname <yanghece96@gmail.com>
2020-11-29 17:42:15 +1100

    merge retrieval prediction into predictor.py

--- ncc/cli/predictor.py
+++ ncc/cli/predictor.py
@@ -35,8 +35,7 @@ def load_state(model_path):
     return args, task, model, use_cuda
 
 
-def main(model_path, input):
-    args, task, model, use_cuda = load_state(model_path)
+def generation_task(args, task, model, use_cuda, input):
     generator = task.build_generator(args)
     # encode input (and feed into gpu)
     input = task.encode_input(input)
@@ -52,15 +51,58 @@ def main(model_path, input):
     return output
 
 
+def retrieval_task(args, task, model, use_cuda, input):
+    # load code_tokens dataset
+    code_dataset = task.load_search_dataset(split=args['dataset']['gen_subset'])
+    # construct similarities
+    import numpy as np
+    similarities = np.zeros(shape=len(code_dataset), dtype=np.float16)
+    # query embeddding
+    query_tokens = task.encode_query_input(input)
+    if use_cuda:
+        query_tokens = utils.move_to_cuda(query_tokens)
+    query_tokens = model.tgt_encoder(query_tokens)
+    # code embeddding
+    for idx, code_tokens in enumerate(code_dataset):
+        if use_cuda:
+            code_tokens = utils.move_to_cuda(code_tokens)
+        code_tokens = code_tokens.unsqueeze(dim=0)
+        code_tokens = model.src_encoder(code_tokens)
+        sim = (query_tokens * code_tokens).sum()
+        similarities[idx] = sim.item()
+    max_idx = np.argmax(similarities)
+    with open(args['eval']['code_file'], 'r') as reader:
+        for idx, line in enumerate(reader):
+            if idx == max_idx:
+                break
+    return line
+
+
+def main(model_path, input):
+    args, task, model, use_cuda = load_state(model_path)
+    if args['common']['task'] in ['summarization', 'be_summarization', 'completion']:
+        return generation_task(args, task, model, use_cuda, input)
+    elif args['common']['task'] in ['retrieval']:
+        return retrieval_task(args, task, model, use_cuda, input)
+    else:
+        raise NotImplementedError(args['common']['task'])
+
+
 def cli_main():
-    # modal_path = '~/.ncc/demo/summarization/neural_transformer/python_wan.pt'
-    modal_path = '~/.ncc/demo/summarization/seq2seq/python_wan.pt'
+    # summarization
+    modal_path = '~/.ncc/demo/summarization/neural_transformer/python_wan.pt'
+    # modal_path = '~/.ncc/demo/summarization/seq2seq/python_wan.pt'
     code = "def positional(max_positional_args):\n\tdef positional_decorator(wrapped):\n\t\t@functools.wraps(wrapped)\n\t\tdef positional_wrapper(*args, **kwargs):\n\t\t\tif (len(args) > max_posi      tional_args):\n\t\t\t\tplural_s = ''\n\t\t\t\tif (max_positional_args != 1):\n\t\t\t\t\tplural_s = 's'\n\t\t\t\tmessage = ('%s()\ttakes\tat\tmost\t%d\tpositional\targument%s\t(%d\tgive      n)' % (wrapped.__name__, max_positional_args, plural_s, len(args)))\n\t\t\t\tif (positional_parameters_enforcement == POSITIONAL_EXCEPTION):\n\t\t\t\t\traise TypeError(message)\n\t\t\t      \telif (positional_parameters_enforcement == POSITIONAL_WARNING):\n\t\t\t\t\tlogger.warning(message)\n\t\t\t\telse:\n\t\t\t\t\tpass\n\t\t\treturn wrapped(*args, **kwargs)\n\t\treturn p      ositional_wrapper\n\tif isinstance(max_positional_args, six.integer_types):\n\t\treturn positional_decorator\n\telse:\n\t\t(args, _, _, defaults) = inspect.getargspec(max_positional_ar      gs)\n\t\treturn positional((len(args) - len(defaults)))(max_positional_args)"
     # ground truth: "a decorator to declare that only the first n arguments my be positional ."
 
+    # completion
     # modal_path = '~/.ncc/demo/completion/seqrnn/py150.pt'
     # code = "body_content = self._serialize.body(parameters, 'ServicePrincipalCreateParameters')\nrequest = self._client.post(url, query_parameters)\nresponse = self._client.send( request, header_parameters, body_content, operation_config)"
     # ground truth: "(request, header_parameters, body_content, **operation_config)"
+
+    # # retrieval
+    # modal_path = "~/.ncc/code_search_net/retrieval/ruby/data-mmap/nbow/checkpoints/checkpoint_best.pt"
+    # code = "Create a missing file if the path is valid."
 
     import argparse
     parser = argparse.ArgumentParser(description="Command Interface")
